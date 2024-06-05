b5cd939d8d18fe548c422fd710c587e0a93eb1ea
ChrisM345 <chrism.12277@gmail.com>
2024-06-05 18:51:31 -0500

    Code cleanup, comments, fixed startGame logic

--- src/components/pokemon-cards.jsx
+++ src/components/pokemon-cards.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import "../styles/cards.css";
+
 let playerCardChoices = [];
+
 function GeneratePokemonCards() {
   const [gameStart, setGameStart] = useState(false);
   const [cardData, setCardData] = useState([]);
@@ -9,10 +11,13 @@ function GeneratePokemonCards() {
   const [bestScore, setBestScore] = useState(0);
 
   function handleStartGame() {
+    //Array with IDs for the original 151 Pokemon
     let pokemonIds = Array.from({ length: 151 }, (_, index) => index + 1);
+    //User chooses 6 to 10 cards to show
     const selectedNumber = parseInt(document.getElementById("card-count").value);
 
     let copiedCardIds = [];
+    //Randomly pick Pokemon IDs and add them to copiedCardIDs. Once all are selected setCardIDs
     for (let i = 0; i < selectedNumber; i++) {
       let pokemonId = Math.floor(Math.random() * pokemonIds.length);
       copiedCardIds.push(pokemonIds[pokemonId]);
@@ -21,32 +26,30 @@ function GeneratePokemonCards() {
     setCardIds(copiedCardIds);
   }
 
+  //Use the pokeapi to grab Pokemon images and names according to the ID
   useEffect(() => {
     const tempData = cardIds.map((id) =>
       fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
         .then((response) => response.json())
         .then((data) => [data.name, data.sprites.other["official-artwork"].front_default])
     );
+
+    //Once we go through all the IDs we setCardData and start the game
     Promise.all(tempData)
       .then((data) => setCardData(data))
-      .then(() => setGameStart(true));
+      .then(() => {
+        if (gameStart == false && cardIds.length > 0) return setGameStart(true);
+      });
   }, [cardIds]);
 
-  // useEffect(() => {
-  //   const tempCardData = cardData;
-  //   for (let i = tempCardData.length - 1; i > 0; i--) {
-  //     const j = Math.floor(Math.random() * (i + 1));
-  //     const temp = tempCardData[i];
-  //     tempCardData[i] = tempCardData[j];
-  //     tempCardData[j] = temp;
-  //   }
-  //   setCardData(tempCardData);
-  // }, [score]);
-
   function handleResetGame() {
+    playerCardChoices.length = 0;
+    setScore(0);
+    setBestScore(0);
     setGameStart(!gameStart);
   }
 
+  //Randomize card location on every click. setCardData to trigger display update
   function randomizeCards() {
     const tempCardData = cardData;
     for (let i = tempCardData.length - 1; i > 0; i--) {
@@ -57,9 +60,11 @@ function GeneratePokemonCards() {
     }
     setCardData(tempCardData);
   }
+
+  //Check player choice by checking choice with elements in array
   function checkPlayerChoice(choice) {
     if (playerCardChoices.includes(choice)) {
-      console.log("you picked this already");
+      //Reset Game
       playerCardChoices.length = 0;
       setScore(0);
       randomizeCards();
@@ -71,7 +76,6 @@ function GeneratePokemonCards() {
       }
       randomizeCards();
     }
-    console.log(playerCardChoices);
   }
 
   function handleClick(cardName) {
@@ -94,12 +98,14 @@ function GeneratePokemonCards() {
     </>
   ) : (
     <>
-      <h2>Test</h2>
       <div className="score">
         Score: {score}
         <br />
         Best Score: {bestScore}
       </div>
+      <button className="reset" onClick={handleResetGame}>
+        Reset
+      </button>
       <div className="card-container">
         {cardData.map((card) => {
           return (
@@ -112,7 +118,6 @@ function GeneratePokemonCards() {
           );
         })}
       </div>
-      <button onClick={handleResetGame}>Reset</button>
     </>
   );
 }
